0ef4aed1019c586326d92c1c6edba0774a1fe63e
Kaden Campbell <82465081+xCymylx@users.noreply.github.com>
2023-03-30 19:08:22 -0600

    modified:   assignment8.tf

--- assignment8.tf
+++ assignment8.tf
@@ -51,6 +51,18 @@ resource "aws_security_group" "tf-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
